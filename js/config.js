070ffc979fefd1ae40742fc15c7151bffd27f028
Kevin Byron <kbyron@bmcc.cuny.edu>
2022-05-10 11:18:05 -0400

    new

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_DtQik6s11', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1denh99091tnohrdpd7fb8tpb5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://bvvirkqix5.execute-api.ap-southeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
